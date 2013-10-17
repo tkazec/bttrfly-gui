015df23227353f20476e85e9735d47f8ef076b06
Teddy Cross <tkazec@gmail.com>
2013-10-17 16:43:09 -0700

    Add custom event delegation and initial file opening.

--- scripts/index.js
+++ scripts/index.js
@@ -18,3 +18,38 @@ var f = ff(function () {
 }).onComplete(function (err) {
 	gui.Window.get().show();
 });
+
+window.addEventListener("dragover", function (evt) {
+	evt.preventDefault();
+}, false);
+
+window.addEventListener("drop", function (evt) {
+	evt.preventDefault();
+	
+	Array.prototype.slice.call(evt.dataTransfer.files).forEach(function (v) {
+		openDirectory(v.path);
+	});
+}, false);
+
+document.getElementById("file").addEventListener("change", function (evt) {
+	openDirectory(this.value);
+}, false);
+
+document.getElementById("root").addEventListener("click", function handle (evt) {
+	var elem = evt._target || evt.target;
+	var orig = evt.target;
+	
+	if (elem.id === "directory-open") {
+		document.getElementById("file").click();
+	} else if (elem.classList.contains("directory-item")) {
+		if (orig.classList.contains("close")) {
+			deleteDirectory(elem.dataset.directory);
+		} else {
+			openDirectory(elem.dataset.directory);
+		}
+	} else {
+		if ((evt._target = cur.parentNode) !== this) {
+			handle(evt);
+		}
+	}
+}, false);
